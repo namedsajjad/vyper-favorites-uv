af974bdc052df7a17159aa9783ff6e797c921207
Mohammad Sajjad Nouri <mhmdsajadno@gmail.com>
2025-09-03 04:54:29 +0330

    Init+ Contract deployment

---
+++ deploy_favorites_unsafe.py
@@ -0,0 +1,44 @@
+import os
+
+from vyper import compile_code
+from web3 import Web3, EthereumTesterProvider
+
+from dotenv import load_dotenv
+load_dotenv()
+
+ADDRESS = os.getenv("MY_ADDRESS")
+PRIVATE = os.getenv("MY_PRIVATE")
+
+def main():
+    print("\n------------------> DEPLOYING...")
+    print(f"Address: {ADDRESS},\nPrivate Key: {PRIVATE}\n")
+    with open("favorites.vy", "r") as f_file:
+        f_code = f_file.read()
+        f_compiled = compile_code(f_code, output_formats=["bytecode", "abi"])
+
+    w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+    f_contract = w3.eth.contract(
+        bytecode=f_compiled["bytecode"], 
+        abi=f_compiled["abi"]
+    )
+
+    nonce = w3.eth.get_transaction_count(ADDRESS)
+    tx = f_contract.constructor().build_transaction(
+        {
+            "from": ADDRESS,
+            "nonce": nonce
+        }
+    )
+    # print(tx)
+
+    signed_tx = w3.eth.account.sign_transaction(tx, private_key=PRIVATE)
+    print(signed_tx)
+    print("\n------------------> Sending TX:")
+
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+    print(f'hash: {tx_hash}')
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    print(f"\n------------------> DONE!\nTX ADDRESS: {tx_receipt.contractAddress}\n")
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from vyper-favorites-uv!")
+
+
+if __name__ == "__main__":
+    main()
